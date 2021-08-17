71c99d6141c83b4164f138a39159b4c72f26f200
vipul71098 <38537174+vipul71098@users.noreply.github.com>
2021-08-17 07:51:50 +0530

    Update Blog.js

--- src/models/Blog.js
+++ src/models/Blog.js
@@ -1,29 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
 
 //Create Schema
 
 const UsersSchema = new Schema({
-        id : {
-		    type: Number,
-		},
-		title : {
-			type:String,
-			required :true
-		},
-		blog: {
-		 	type: String,
-		 	required : true
-		},
-		comments: {
-		 	type: String,
-		},
-	     img: {
-		   data: String,
-		   contentType: String,
-		 },
-
+  id: {
+    type: Number
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  blog: {
+    type: String,
+    required: true
+  },
+  comments: {
+    type: String
+  },
+  img: {
+    data: String,
+    contentType: String
+  }
 });
 
-module.exports = Blogs = mongoose.model('blog',UsersSchema);
+module.exports = Blogs = mongoose.model("blog", UsersSchema);
