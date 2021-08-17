c8846cf0e4769cd7e59db768838faad0fd4ade86
vipul71098 <38537174+vipul71098@users.noreply.github.com>
2021-08-17 07:52:16 +0530

    Update blogroute.js

--- src/routes/blogroute.js
+++ src/routes/blogroute.js
@@ -1,12 +1,16 @@
-const express = require('express')
-const imageUpload = require('../middleware/upload');
-const router = express.Router()
-const blogController = require('../controllers/blogcontroller')
+const express = require("express");
+const imageUpload = require("../middleware/upload");
+const router = express.Router();
+const blogController = require("../controllers/blogcontroller");
 
-router.post('/insertblog', imageUpload.single('image'), blogController.insertBlog)
-router.put('/updateblog/:id', blogController.UpdateBLog)
-router.get('/getblog', blogController.getBLog)
-router.get('/getsingleblog/:id', blogController.getSingleBLog)
-router.delete('/deleteblog/:id', blogController.DeleteBLog)
+router.post(
+  "/insertblog",
+  imageUpload.single("image"),
+  blogController.insertBlog
+);
+router.put("/updateblog/:id", blogController.UpdateBLog);
+router.get("/getblog", blogController.getBLog);
+router.get("/getsingleblog/:id", blogController.getSingleBLog);
+router.delete("/deleteblog/:id", blogController.DeleteBLog);
 
-module.exports = router
+module.exports = router;
