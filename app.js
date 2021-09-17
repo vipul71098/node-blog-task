f2ae7cb94c158e98294508e5b521727d63d2d678
vipul71098 <38537174+vipul71098@users.noreply.github.com>
2021-09-17 18:40:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const corsOptions = {
-  origin: "*"
-};
 
-const app = express();
+app.use(cors());
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+});
+
+
 const port = process.env.PORT || 9010;
 
 const db = require("./config/keys").mongoURI;
